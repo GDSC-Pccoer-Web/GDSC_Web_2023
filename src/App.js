2207851bcb0c7c61785b6ee5374c798238cc84e5
Vedantaz <muleyvedant4@gmail.com>
2023-02-20 16:22:27 +0530

    added footer and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 }
